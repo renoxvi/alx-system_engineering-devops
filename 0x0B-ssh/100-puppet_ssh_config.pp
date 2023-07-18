ce1b9a49f521a902ed5e259f70a9ae8234d047ef
renoxvi <vicreno08@gmail.com>
2023-07-18 12:02:45 -0700

    puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # setup client SSH configuration
 
-file { 'etc/ssh/ssh_config':
+file { 'ect/ssh/ssh_config':
 	ensure => present,
 
 content =>"
@@ -11,4 +11,5 @@ content =>"
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
 	",
+
 }
