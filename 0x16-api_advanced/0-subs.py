ac2bf3cd4242141ea2e6568eaba8368a3490d2ea
renoxvi <vicreno08@gmail.com>
2023-09-21 02:49:32 +0300

    j

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Function that queries the Reddit API """
 import requests
-import sys
-
 
 def number_of_subscribers(subreddit):
     """  Args:
@@ -11,9 +9,14 @@ def number_of_subscribers(subreddit):
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
     headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+        return response.json().get("data").get("subscribers")
+    return 0
+
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit: ")
+    subscribers = number_of_subscribers(subreddit)
+    print(f"Number of subscribers to the subreddit: {subscribers}")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,22 +4,24 @@ Query Reddit API for titles of top ten posts of a given subreddit
 """
 import requests
 
-
 def top_ten(subreddit):
     """
         return top ten titles for a given subreddit
         return None if invalid subreddit given
     """
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
+    # Set user agent
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    r = requests.get(url, headers=headers).json()
-    top_ten = r.get('data', {}).get('children', [])
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    response = requests.get(url, headers=headers).json()
+    top_ten = response.get('data', {}).get('children', [])
+
     if not top_ten:
         print(None)
-    for t in top_ten:
-        print(t.get('data').get('title'))
+
+    for post in top_ten:
+        print(post.get('data').get('title'))
+
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit: ")
+    top_ten(subreddit)
